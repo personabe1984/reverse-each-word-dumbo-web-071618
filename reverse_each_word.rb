b0b3011d0465bd5e5942293808e7dba99190857a
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-04 17:26:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sen)
   array_of_sen.collect do |word|
     word.reverse!
   end
-  
+  return array_of_sen.join("")
 end
 
 puts reverse_each_word("Hello there, and how are you?")
